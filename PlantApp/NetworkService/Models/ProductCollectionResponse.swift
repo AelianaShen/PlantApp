7fabceaa0795bfbb1f8cc6dfc0af8691910a3521
aeliana <poshen@oregonstate.edu>
2024-03-12 21:30:33 -0700

    removed metadata

--- PlantApp/NetworkService/Models/ProductCollectionResponse.swift
+++ PlantApp/NetworkService/Models/ProductCollectionResponse.swift
@@ -8,29 +8,13 @@
 import Foundation
 
 struct ProductCollectionResponse: Codable {
-    let metadata: Metadata
     let count: Int
     let plants: [Plant]
     let scannedCount: Int
 
     enum CodingKeys: String, CodingKey {
-        case metadata = "$metadata"
         case count = "Count"
         case plants = "Items"
         case scannedCount = "ScannedCount"
-    }
-}
-
-extension ProductCollectionResponse {
-    struct Metadata: Codable {
-        let httpStatusCode: Int
-        let requestID: String
-        let attempts, totalRetryDelay: Int
-
-        enum CodingKeys: String, CodingKey {
-            case httpStatusCode
-            case requestID = "requestId"
-            case attempts, totalRetryDelay
-        }
     }
 }

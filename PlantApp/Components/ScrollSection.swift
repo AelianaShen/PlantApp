fc73047f854be67906774917b1e3c555a22d18f0
aeliana <poshen@oregonstate.edu>
2024-03-14 12:22:55 -0700

    when recommend list has no data, show the status on the screen

--- PlantApp/Components/ScrollSection.swift
+++ PlantApp/Components/ScrollSection.swift
@@ -15,20 +15,24 @@ struct ScrollSection: View {
     
     var body: some View {
         VStack (alignment: .leading) {
-            Text("Recommand list for you")
+            Text("Recommended list for you")
                 .font(.headline)
                 .foregroundColor(ProjColor.Gumleaf)
                 .padding(.horizontal, 25.0)
             
             ScrollView(.horizontal, showsIndicators: false){
                 HStack {
-                    ForEach(plantList ?? Plant.localPlantList, id: \.productID) { product in
+                    ForEach(plantList ?? [], id: \.productID) { product in
                         NavigationLink {
                             ProductDetailView(plant: product)
                         } label: {
                             PlantCard(plant: product)
                         }
-                        
+                    }
+                    if (plantList == nil) {
+                        Text("The recommended list has no data.")
+                            .font(.headline)
+                            .foregroundColor(ProjColor.PrimaryGreen)
                     }
                 }
                 .padding(50)

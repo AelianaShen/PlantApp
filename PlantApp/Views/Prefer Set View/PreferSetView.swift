721b1504b8944010c75657b98b72e4c0373b3533
aeliana <poshen@oregonstate.edu>
2024-03-22 14:53:52 -0700

    refactor PreferSetView

---
+++ PlantApp/Views/Prefer Set View/PreferSetView.swift
@@ -0,0 +1,48 @@
+//
+//  PreferSetView.swift
+//  PlantCodeiOS15
+//
+//  Created by Aeliana Shen on 6/5/23.
+//
+
+import SwiftUI
+
+struct PreferSetView: View {
+    @StateObject var viewModel = PreferSetViewModel()
+    
+    var body: some View {
+        NavigationView {
+            VStack(spacing: 20) {
+                plantDevImage
+                guideline
+                startQuestionsButton
+            }
+            .padding(.horizontal, 40)
+        }
+    }
+    
+    private var plantDevImage: some View {
+        Image("plant_dev_icon")
+    }
+    
+    private var guideline: some View {
+        Text(viewModel.guidelineString)
+            .multilineTextAlignment(.center)
+            .foregroundColor(.secondary)
+    }
+    
+    private var startQuestionsButton: some View {
+        NavigationLink {
+            QuestionView()
+                .environmentObject(viewModel.questionManager)
+        } label: {
+            PrimaryButton(text: viewModel.primaryButtonString)
+        }
+    }
+}
+
+struct PreferSetView_Previews: PreviewProvider {
+    static var previews: some View {
+        PreferSetView()
+    }
+}

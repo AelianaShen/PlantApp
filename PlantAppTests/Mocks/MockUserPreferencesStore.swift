c089afa92817ff7e9a34a669ca0c49a3eb733e6d
aeliana <poshen@oregonstate.edu>
2024-04-04 16:38:58 -0700

    update test code: use enum in UserPreferencesStore

--- PlantAppTests/Mocks/MockUserPreferencesStore.swift
+++ PlantAppTests/Mocks/MockUserPreferencesStore.swift
@@ -9,12 +9,12 @@ import Foundation
 @testable import PlantApp
 
 class MockUserPreferencesStore: UserPreferencesStore {
-    var colorScheme: String? = nil
-    var maintenanceLevel: String? = nil
-    var careLevel: String? = nil
-    var leafStyle: String? = nil
-    var lightLevel: Int? = nil
-    var boxLevel: Int? = nil
-    var petFriendly: String? = nil
-    var productiveOrDecorative: String? = nil
+    var careLevel: UserPreferences.CareLevel? = nil
+    var leafStyle: UserPreferences.LeafStyle? = nil
+    var lightLevel: UserPreferences.LightLevel? = nil
+    var boxLevel: UserPreferences.BoxLevel? = nil
+    var petFriendly: UserPreferences.PetFriendly? = nil
+    var colorScheme: UserPreferences.ColorScheme? = nil
+    var maintenanceLevel: UserPreferences.MaintenanceLevel? = nil
+    var productiveOrDecorative: UserPreferences.ProductiveOrDecorative? = nil
 }

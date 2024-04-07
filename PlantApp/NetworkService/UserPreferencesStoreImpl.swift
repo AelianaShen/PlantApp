c941b105fde6e3d2d4456072d2c3fbd95ceb0bb7
aeliana <poshen@oregonstate.edu>
2024-04-07 02:18:14 -0700

    rename userPreferData to userDefaults, add init

--- PlantApp/NetworkService/UserPreferencesStoreImpl.swift
+++ PlantApp/NetworkService/UserPreferencesStoreImpl.swift
@@ -19,103 +19,107 @@ protocol UserPreferencesStore {
 }
 
 class UserPreferencesStoreImpl: UserPreferencesStore {
-    private let userPreferData = UserDefaults.standard
+    private let userDefaults : UserDefaults
+    
+    init(userDefaults: UserDefaults = UserDefaults.standard) {
+        self.userDefaults = userDefaults
+    }
     
     var careLevel: UserPreferences.CareLevel? {
         get {
-            let careLevelString = userPreferData.string(forKey: "careLevel")
+            let careLevelString = userDefaults.string(forKey: "careLevel")
             if let careLevelString {
                 return UserPreferences.CareLevel(rawValue: careLevelString)
             }
             return nil
         }
         set {
-            userPreferData.set(newValue?.rawValue, forKey: "careLevel")
+            userDefaults.set(newValue?.rawValue, forKey: "careLevel")
         }
     }
     
     var leafStyle: UserPreferences.LeafStyle? {
         get {
-            let leafStyleString = userPreferData.string(forKey: "leafStyle")
+            let leafStyleString = userDefaults.string(forKey: "leafStyle")
             if let leafStyleString {
                 return UserPreferences.LeafStyle(rawValue: leafStyleString)
             }
             return nil
         }
         set {
-            userPreferData.set(newValue?.rawValue, forKey: "leafStyle")
+            userDefaults.set(newValue?.rawValue, forKey: "leafStyle")
         }
     }
     
     var lightLevel: UserPreferences.LightLevel? {
         get {
-            let lightLevelInt = userPreferData.integer(forKey: "lightLevel")
+            let lightLevelInt = userDefaults.integer(forKey: "lightLevel")
             return UserPreferences.LightLevel(rawValue: lightLevelInt)
         }
         set {
-            userPreferData.set(newValue?.rawValue, forKey: "lightLevel")
+            userDefaults.set(newValue?.rawValue, forKey: "lightLevel")
         }
     }
     
     var boxLevel: UserPreferences.BoxLevel? {
         get {
-            let boxLevelInt = userPreferData.integer(forKey: "boxLevel")
+            let boxLevelInt = userDefaults.integer(forKey: "boxLevel")
             return UserPreferences.BoxLevel(rawValue: boxLevelInt)
         }
         set {
-            userPreferData.set(newValue?.rawValue, forKey: "boxLevel")
+            userDefaults.set(newValue?.rawValue, forKey: "boxLevel")
         }
     }
     
     var petFriendly: UserPreferences.PetFriendly? {
         get {
-            let petFriendlyString = userPreferData.string(forKey: "petFriendly")
+            let petFriendlyString = userDefaults.string(forKey: "petFriendly")
             if let petFriendlyString {
                 return UserPreferences.PetFriendly(rawValue: petFriendlyString)
             }
             return nil
         }
         set {
-            userPreferData.set(newValue?.rawValue, forKey: "petFriendly")
+            userDefaults.set(newValue?.rawValue, forKey: "petFriendly")
         }
     }
     
     var colorScheme: UserPreferences.ColorScheme? {
         get {
-            let colorSchemeString = userPreferData.string(forKey: "colorScheme")
+            let colorSchemeString = userDefaults.string(forKey: "colorScheme")
             if let colorSchemeString {
                 return UserPreferences.ColorScheme(rawValue: colorSchemeString)
             }
             return nil
         }
         set {
-            userPreferData.set(newValue?.rawValue, forKey: "colorScheme")
+            userDefaults.set(newValue?.rawValue, forKey: "colorScheme")
         }
     }
     
     var maintenanceLevel: UserPreferences.MaintenanceLevel? {
         get {
-            let maintenanceLevelString = userPreferData.string(forKey: "maintenanceLevel")
+            let maintenanceLevelString = userDefaults.string(forKey: "maintenanceLevel")
             if let maintenanceLevelString {
                 return UserPreferences.MaintenanceLevel(rawValue: maintenanceLevelString)
             }
             return nil
         }
         set {
-            userPreferData.set(newValue?.rawValue, forKey: "maintenanceLevel")
+            userDefaults.set(newValue?.rawValue, forKey: "maintenanceLevel")
         }
     }
     
     var productiveOrDecorative: UserPreferences.ProductiveOrDecorative? {
         get {
-            let productiveOrDecorativeString = userPreferData.string(forKey: "productiveOrDecorative")
+            let productiveOrDecorativeString = userDefaults.string(forKey: "productiveOrDecorative")
             if let productiveOrDecorativeString {
                 return UserPreferences.ProductiveOrDecorative(rawValue: productiveOrDecorativeString)
             }
             return nil
         }
         set {
-            userPreferData.set(newValue?.rawValue, forKey: "productiveOrDecorative")
+            userDefaults.set(newValue?.rawValue, forKey: "productiveOrDecorative")
         }
     }
 }

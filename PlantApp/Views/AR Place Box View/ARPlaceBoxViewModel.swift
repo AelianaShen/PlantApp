e00b898c753665a6f3c55db8645a5014bc4b4140
aeliana <poshen@oregonstate.edu>
2024-04-07 09:42:27 -0700

    apply lightLevel and boxLevel type

--- PlantApp/Views/AR Place Box View/ARPlaceBoxViewModel.swift
+++ PlantApp/Views/AR Place Box View/ARPlaceBoxViewModel.swift
@@ -13,11 +13,11 @@ class ARPlaceBoxViewModel: ObservableObject {
     @Published var showConfirmMsg: Bool = false
     
     @Published var luxValue: CGFloat = 0
-    var lightLevel: Int = UserPreferences.LightLevel.none.rawValue
+    var lightLevel: UserPreferences.LightLevel = UserPreferences.LightLevel.none
     var confirmLuxValue: CGFloat = 0
     
     @Published var boxSize: SIMD3<Float> = simd_float3(0, 0, 0)
-    var boxLevel: Int = UserPreferences.BoxLevel.none.rawValue
+    var boxLevel: UserPreferences.BoxLevel = UserPreferences.BoxLevel.none
     var confirmBoxSize: SIMD3<Float>  = simd_float3(0, 0, 0)
     
     var useARKit = false
@@ -30,36 +30,36 @@ class ARPlaceBoxViewModel: ObservableObject {
     
     func saveLuxValue() {
         if confirmLuxValue > 25, confirmLuxValue <= 75 {
-            lightLevel = UserPreferences.LightLevel.low.rawValue
+            lightLevel = UserPreferences.LightLevel.low
         } else if confirmLuxValue > 75, confirmLuxValue <= 200 {
-            lightLevel = UserPreferences.LightLevel.medium.rawValue
+            lightLevel = UserPreferences.LightLevel.medium
         } else if confirmLuxValue > 200 {
-            lightLevel = UserPreferences.LightLevel.high.rawValue
+            lightLevel = UserPreferences.LightLevel.high
         } else {
-            lightLevel = UserPreferences.LightLevel.none.rawValue
+            lightLevel = UserPreferences.LightLevel.none
         }
-        userPreferences.lightLevel = UserPreferences.LightLevel(rawValue: lightLevel)
+        userPreferences.lightLevel = lightLevel
     }
     
     func clearLuxValue() {
-        userPreferences.lightLevel = UserPreferences.LightLevel(rawValue: 0)
+        userPreferences.lightLevel = UserPreferences.LightLevel.none
     }
     
     func saveBoxSize() {
         let plantPotDiameter = confirmBoxSize.x
         if plantPotDiameter <= 0.2 {
-            boxLevel = UserPreferences.BoxLevel.small.rawValue
+            boxLevel = UserPreferences.BoxLevel.small
         } else if plantPotDiameter > 0.2, plantPotDiameter <= 0.32 {
-            boxLevel = UserPreferences.BoxLevel.medium.rawValue
+            boxLevel = UserPreferences.BoxLevel.medium
         } else if plantPotDiameter > 0.32 {
-            boxLevel = UserPreferences.BoxLevel.large.rawValue
+            boxLevel = UserPreferences.BoxLevel.large
         } else {
-            boxLevel = UserPreferences.BoxLevel.none.rawValue
+            boxLevel = UserPreferences.BoxLevel.none
         }
-        userPreferences.boxLevel = UserPreferences.BoxLevel(rawValue: boxLevel)
+        userPreferences.boxLevel = boxLevel
     }
     
     func clearBoxSize() {
-        userPreferences.boxLevel = UserPreferences.BoxLevel(rawValue: 0)
+        userPreferences.boxLevel = UserPreferences.BoxLevel.none
     }
 }
